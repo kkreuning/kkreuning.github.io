8d007e423d626f53a62cd95f8f963f85806b6c07
Kay Kreuning <kay.kreuning@lunatech.nl>
2019-05-07 10:59:10 +0200

    Add jekyll setup.
    Used minimal mistakes (https://mmistakes.github.io/minimal-mistakes) for theme.

---
+++ _posts/2019-05-07-hello-world.md
@@ -0,0 +1,7 @@
+---
+layout: posts
+title: Hello World
+---
+
+This is my first post on my new blog. I will be writing about programming and
+technology mostly.
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Hello World",
+        "excerpt":"This is my first post on my new blog. I will be writing about programming and technology mostly.  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/hello-world/",
+        "teaser":null}]
