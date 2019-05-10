3fec95b4d9575254d87c1839ffbf13a4b09e73eb
Kay Kreuning <kay.kreuning@lunatech.nl>
2019-05-10 10:26:08 +0200

    Enable Google Analytics.

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,10 +2,10 @@ var store = [{
         "title": "Hello World",
         "excerpt":"This is my first post on my new blog. I will be writing about programming and technology mostly.  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/hello-world/",
+        "url": "https://kkreuning.github.io/hello-world/",
         "teaser":null},{
         "title": "Guardrail with http4s tutorial",
         "excerpt":"This article is an introduction on how to use Twilio’s Guardrail to safely generate and maintain a http4s REST API server. I wanted to write this article as a reference for my future self and others who are interested in this technology. tl;dr - I just want the code! Find...","categories": ["article"],
         "tags": ["scala","guardrail","http4s","tutorial"],
-        "url": "http://localhost:4000/article/guardrail-with-http4s-tutorial/",
+        "url": "https://kkreuning.github.io/article/guardrail-with-http4s-tutorial/",
         "teaser":null}]
